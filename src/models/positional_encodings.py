a8790fce91b02f6bc221c79467b5266da7eb1970
Trong Thang Pham <phamtrongthang123@gmail.com>
2025-11-30 22:44:14 -0600

    typo

--- src/models/positional_encodings.py
+++ src/models/positional_encodings.py
@@ -1,9 +1,20 @@
 import math
+
 import torch
 from torch import nn
 
+
 class PositionEmbeddingSine3d(nn.Module):
-    def __init__(self, spatial_dim, hidden_dim=768, temperature=10000, normalize=False, scale=None, flatten = True, device = "cuda:0"):
+    def __init__(
+        self,
+        spatial_dim,
+        hidden_dim=768,
+        temperature=10000,
+        normalize=False,
+        scale=None,
+        flatten=True,
+        device="cuda:0",
+    ):
         super(PositionEmbeddingSine3d, self).__init__()
         self.num_pos_feats = hidden_dim // 3
         normalize = normalize
@@ -21,7 +32,10 @@ class PositionEmbeddingSine3d(nn.Module):
             position_y = position_y / (self.h - 1 + eps) * scale
             position_x = position_x / (self.w - 1 + eps) * scale
             position_z = position_z / (self.c - 1 + eps) * scale
-        div_term = torch.exp(torch.arange(0, self.num_pos_feats, 2) * (-math.log(temperature) / self.num_pos_feats))
+        div_term = torch.exp(
+            torch.arange(0, self.num_pos_feats, 2)
+            * (-math.log(temperature) / self.num_pos_feats)
+        )
         pe_y = torch.zeros(self.h, 1, 1, self.num_pos_feats)
         pe_x = torch.zeros(1, self.w, 1, self.num_pos_feats)
         pe_z = torch.zeros(1, 1, self.c, self.num_pos_feats)
@@ -30,7 +44,7 @@ class PositionEmbeddingSine3d(nn.Module):
         pe_y[:, 0, 0, 1::2] = torch.cos(position_y * div_term)
         pe_x[0, :, 0, 0::2] = torch.sin(position_x * div_term)
         pe_x[0, :, 0, 1::2] = torch.cos(position_x * div_term)
-        pe_z[0, 0, :, 1::2] = torch.sin(position_z * div_term)
+        pe_z[0, 0, :, 0::2] = torch.sin(position_z * div_term)
         pe_z[0, 0, :, 1::2] = torch.cos(position_z * div_term)
         pe_y = pe_y.repeat(1, self.w, self.c, 1)
         pe_x = pe_x.repeat(self.h, 1, self.c, 1)
@@ -39,7 +53,7 @@ class PositionEmbeddingSine3d(nn.Module):
             3, 0, 1, 2
         )  # dim, h, w, c
         if flatten:
-            self.pos =  self.pos.view(hidden_dim, -1).permute(1,0).unsqueeze(1)
+            self.pos = self.pos.view(hidden_dim, -1).permute(1, 0).unsqueeze(1)
         else:
             self.pos = self.pos.permute(1, 2, 3, 0)
         self.pos = nn.Parameter(self.pos, requires_grad=False)
